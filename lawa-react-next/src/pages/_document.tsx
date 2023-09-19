82fe1ec54d468e34d52b474d43ce9324ba93e7c7
Roman Sapezhko <d-w-b@mail.ru>
2023-09-19 12:15:42 +0300

    Update _document.tsx
    Changed GTM codes

--- lawa-react-next/src/pages/_document.tsx
+++ lawa-react-next/src/pages/_document.tsx
@@ -10,7 +10,19 @@ class MyDocument extends Document {
     return (
       <Html lang="ru">
         <Head>
-          <body>
+          <script dangerouslySetInnerHTML={{
+              __html: `
+                   (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N3HKN3ZP');`,
+            }}></script>
+        </Head>
+        <body>
+            <noscript dangerouslySetInnerHTML={{ __html: `https://www.googletagmanager.com/ns.html?id=GTM-N3HKN3ZP"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
+
             <Main />
             <NextScript />
             <script dangerouslySetInnerHTML={{
@@ -20,7 +32,7 @@ class MyDocument extends Document {
                    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
                    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
                    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-                
+
                    ym(88485289, "init", {
                         clickmap:true,
                         trackLinks:true,
@@ -35,18 +47,7 @@ class MyDocument extends Document {
                 />
               </div>
             </noscript>
-            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-250028760-1"></script>
-            <script dangerouslySetInnerHTML={{
-              __html:
-
-                `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                            'https://www.googletagmanager.com/gtag/js?id=UA-250028760-1'+i+dl;f.parentNode.insertBefore(j,f);
-                            })(window,document,'script','dataLayer','GTM-250028760-1');`
-            }} />
           </body>
-        </Head>
       </Html>
     );
   }
